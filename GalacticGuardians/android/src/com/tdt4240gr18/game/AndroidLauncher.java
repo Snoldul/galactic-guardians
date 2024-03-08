2f3117a3bcaac3304816382eb675323a1114d056
jorgolsl <jorgolsl@stud.ntnu.no>
2024-03-08 13:12:39 +0100

    Initial setup

---
+++ GalacticGuardians/android/src/com/tdt4240gr18/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.tdt4240gr18.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.tdt4240gr18.game.GalacticGuardians;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GalacticGuardians(), config);
+	}
+}
