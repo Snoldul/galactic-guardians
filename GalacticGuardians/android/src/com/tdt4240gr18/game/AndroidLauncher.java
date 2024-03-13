2579514dbfdd775220b4717161845763d0eaf5c2
Johannes <johmo@stud.ntnu.no>
2024-03-13 22:35:57 +0100

    Include firebase support. Tried for ever to include testing but didn't get it to work.

--- GalacticGuardians/android/src/com/tdt4240gr18/game/AndroidLauncher.java
+++ GalacticGuardians/android/src/com/tdt4240gr18/game/AndroidLauncher.java
@@ -1,16 +1,28 @@
 package com.tdt4240gr18.game;
 
 import android.os.Bundle;
-
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.analytics.FirebaseAnalytics;
 import com.tdt4240gr18.game.GalacticGuardians;
 
 public class AndroidLauncher extends AndroidApplication {
+	private FirebaseAnalytics firebaseAnalytics;
+
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new GalacticGuardians(), config);
+		FirebaseApp.initializeApp(this);
+
+		firebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+		// Log an event
+		Bundle bundle = new Bundle();
+		bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "1");
+		bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "test_event");
+		firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
 	}
 }
