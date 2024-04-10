ca03484b9748e65dc70f8e62d0f27c3834f3fa37
Johannes <johmo@stud.ntnu.no>
2024-04-10 19:15:41 +0200

    chore(#15) Added leaderboard entries
    Added leaderboard state that shows scores of placeholder entries. Added dynamic implementaion of dots to help user see scores of different players. Added sorting of list based on score.
    
    TODO: Implement data querying to retrieve actual data from firebase.

--- GalacticGuardians/core/src/states/LeaderboardState.java
+++ GalacticGuardians/core/src/states/LeaderboardState.java
@@ -5,11 +5,12 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Rectangle;
 import com.tdt4240gr18.game.LeaderboardEntry;
 import com.tdt4240gr18.game.MenuButton;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 
@@ -18,14 +19,15 @@ public class LeaderboardState extends State{
     private final List<MenuButton> buttons;
     private final int width;
     public int height;
-    private final int BUTTON_OFFSET;
+    private final int buttonOffsetY;
+    private final int entryoffsetX, entryMargin;
     private final BitmapFont font;
     private GlyphLayout layout;
     //private final Rectangle bounds;
     private final Texture Backdrop;
     private List<LeaderboardEntry> EntriesList;
     // TEMPORARY:
-    LeaderboardEntry temp1, temp2, temp3, temp4;
+    LeaderboardEntry temp1, temp2, temp3, temp4, temp5, temp6, temp7, temp8, temp9, temp0;
 
 
 
@@ -36,7 +38,8 @@ public class LeaderboardState extends State{
         height = Gdx.graphics.getHeight();
         isAndr = isAndroid();
         buttons = new ArrayList<>();
-        BUTTON_OFFSET = 20;
+        EntriesList = new ArrayList<>();
+        buttonOffsetY = 20;
         buttons.add(addButton("Back"));
         if (isAndr) {
             buttons.add(addButton("Log in"));
@@ -44,16 +47,33 @@ public class LeaderboardState extends State{
         this.font = new BitmapFont(Gdx.files.internal("RetroTitle.fnt"));
         //this.bounds = new Rectangle(x, y, texture.getWidth(), texture.getHeight());
         Backdrop = new Texture("backdrop1x2.png");
+        entryoffsetX = (width - Backdrop.getWidth()) / 2;
+        entryMargin = 60;
 
         // TEMPORARY:
-/*        temp1 = new LeaderboardEntry("B01", 100);
+        temp1 = new LeaderboardEntry("B01", 100);
         temp2 = new LeaderboardEntry("B02", 200);
         temp3 = new LeaderboardEntry("B03", 50);
         temp4 = new LeaderboardEntry("B04", 400);
+        temp5 = new LeaderboardEntry("B05", 400);
+        temp6 = new LeaderboardEntry("B06", 0);
+        temp7 = new LeaderboardEntry("B07", 10);
+        temp8 = new LeaderboardEntry("B08", 4000);
+        temp9 = new LeaderboardEntry("B09", 420);
+        temp0 = new LeaderboardEntry("B00000", 400);
         EntriesList.add(temp1);
         EntriesList.add(temp2);
         EntriesList.add(temp3);
-        EntriesList.add(temp4);*/
+        EntriesList.add(temp4);
+        EntriesList.add(temp5);
+        EntriesList.add(temp6);
+        EntriesList.add(temp7);
+        EntriesList.add(temp8);
+        EntriesList.add(temp9);
+        EntriesList.add(temp0);
+
+        //To sort according to score
+        sortList((ArrayList<LeaderboardEntry>) EntriesList);
     }
 
     private boolean isAndroid() {
@@ -69,8 +89,17 @@ public class LeaderboardState extends State{
     private MenuButton addButton(String text) {
         Texture buttonTexture = new Texture("menuBtn.png");
         float x = (float) (width - buttonTexture.getWidth()) / 2;
-        float y = (float) BUTTON_OFFSET + buttons.size() * (buttonTexture.getHeight() + BUTTON_OFFSET);
+        float y = (float) buttonOffsetY + buttons.size() * (buttonTexture.getHeight() + buttonOffsetY);
         return new MenuButton(buttonTexture, text, x, y);
+    }
+
+    private void sortList(ArrayList<LeaderboardEntry> list) {
+        Collections.sort(EntriesList, new Comparator<LeaderboardEntry>() {
+            @Override
+            public int compare(LeaderboardEntry t1, LeaderboardEntry t2) {
+                return t2.getScore() - t1.getScore();
+            }
+        });
     }
 
     @Override
@@ -101,14 +130,19 @@ public class LeaderboardState extends State{
 
     @Override
     public void render(SpriteBatch sb) {
+        font.getData().setScale(2, 2);
         sb.begin();
         //this.layout = new GlyphLayout(font, buttonText);
-        sb.draw(Backdrop, (float) width /2 - (float) Backdrop.getWidth() / 2, height - (Backdrop.getHeight() + BUTTON_OFFSET));
+        sb.draw(Backdrop, (float) width /2 - (float) Backdrop.getWidth() / 2, height - (Backdrop.getHeight() + buttonOffsetY));
         if (isAndr) {
 
             }
         for (MenuButton button : buttons) {
             button.render(sb);
+        }
+        for (int i = 0; i < EntriesList.size(); i++) {
+            //EntriesList.get(i).render(sb,font, entryoffsetX + entryMargin, height - 100 - (10) * i, Backdrop.getWidth() - entryMargin * 2);
+            EntriesList.get(i).render(sb,font, entryoffsetX + entryMargin, height - 100 - ((int) font.getLineHeight() + buttonOffsetY) * i, Backdrop.getWidth() - entryMargin * 2);
         }
         sb.end();
     }
