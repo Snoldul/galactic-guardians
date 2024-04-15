81cba2a31bacd325e7fd8b7a03fc811e3db9805a
Caroline <carolisb@stud.ntnu.no>
2024-04-15 20:21:04 +0200

    feat(#19): Add options functionality to OptionsState
    The class now initializes options. Input handling allows users to toggle options, and rendering methods are provided for displaying the options. Implemented proper disposal of resources.

--- GalacticGuardians/core/src/states/OptionsState.java
+++ GalacticGuardians/core/src/states/OptionsState.java
@@ -6,44 +6,82 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
+import com.tdt4240gr18.game.Option;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class OptionsState extends State{
-    private final BitmapFont fontTitle;
-    private final BitmapFont fontText;
+    private static final float OPTION_OFFSET = 20f;
+    private static final float TEXT_SCALE = 3f;
+    private static final String TITLE_TEXT = "Options";
+
+    private BitmapFont fontTitle;
+    private final List<Option> options = new ArrayList<>();
+
     private GlyphLayout layout;
     private Texture optionsMenu;
     private Rectangle xBtnBounds;
     private Texture xBtn;
     private int width;
     private int height;
-    private final String titleText;
     private float newHeight;
     private float newWidth;
     private float x;
     private float y;
+    private float optionHeight;
+    private Texture onTexture;
+    private Texture offTexture;
 
     public OptionsState(GameStateManager gsm) {
         super(gsm);
+        initializeTextures();
+        initializeDimensions();
+        initializeFont();
+        initializeOptions();
+        initializeXButtonBounds();
+    }
+
+    public void initializeTextures(){
         optionsMenu = new Texture("optionsBackground.png");
         xBtn = new Texture("xBtn.png");
+        onTexture = new Texture("onBtn.png");
+        offTexture = new Texture("offBtn.png");
+    }
+
+    public void initializeDimensions(){
         width = Gdx.graphics.getWidth();
         height = Gdx.graphics.getHeight();
-        fontTitle = new BitmapFont(Gdx.files.internal("RetroTitle.fnt"));
-        fontText = new BitmapFont(Gdx.files.internal("RetroTitle.fnt"));
-        titleText = "Options";
-        float textScale = 3;
-        fontTitle.getData().setScale(textScale);
-
         newWidth = width * 0.95f;
         newHeight = ((float) optionsMenu.getHeight() / (float) optionsMenu.getWidth()) * newWidth;
         x = (width - newWidth) / 2;
         y = (height - newHeight) / 2 + height * 0.05f;
+        optionHeight = height * 0.1f;
+    }
+
+    private void initializeFont(){
+        fontTitle = new BitmapFont(Gdx.files.internal("RetroTitle.fnt"));
+        fontTitle.getData().setScale(TEXT_SCALE);
+    }
+
+    public void initializeOptions(){
+        addOption("Music");
+        addOption("Sound");
+        addOption("Tutorial");
+    }
+
+    private void initializeXButtonBounds(){
         float xBtnWidth = width * 0.15f;
         float xBtnHeight = xBtnWidth;
         float xButtonX = (width - xBtnWidth) / 2;
         float xButtonY = (height - y - newHeight) + xBtnHeight;
         xBtnBounds = new Rectangle(xButtonX, xButtonY, xBtnWidth, xBtnHeight);
-
+    }
+    public void addOption(String name) {
+        float additionalOffset = 350f;
+        float optionY = y + newHeight - (options.size() + 1) * (optionHeight + OPTION_OFFSET) - additionalOffset;
+        Rectangle bounds = new Rectangle(x, optionY, width, optionHeight);
+        options.add(new Option(name, onTexture, offTexture, bounds));
     }
 
     @Override
@@ -53,6 +91,11 @@ public class OptionsState extends State{
             float y = height - Gdx.input.getY();
             if (xBtnBounds.contains(x, y)) {
                 gsm.pop();
+            }
+            for (Option option : options){
+                if (option.contains(x,y)){
+                    option.toggle();
+                }
             }
         }
 
@@ -66,21 +109,39 @@ public class OptionsState extends State{
     @Override
     public void render(SpriteBatch sb) {
         sb.begin();
-        sb.draw(optionsMenu, x, y, newWidth, newHeight);
+        renderOptionsMenu(sb);
+        renderTitle(sb);
+        renderOptions(sb);
+        sb.end();
+    }
+
+    private void renderOptionsMenu(SpriteBatch sb){
+        sb.draw(optionsMenu,x, y, newWidth, newHeight);
         sb.draw(xBtn, xBtnBounds.x, xBtnBounds.y, xBtnBounds.width, xBtnBounds.height);
-        layout = new GlyphLayout(fontTitle, titleText);
+    }
+
+    private void renderTitle(SpriteBatch sb){
+        layout = new GlyphLayout(fontTitle, TITLE_TEXT);
         float titleWidth = layout.width;
         float titleHeight = layout.height;
-
         float titleX = x + ((newWidth - titleWidth) / 2);
         float titleY = y + newHeight - titleHeight - newHeight * 0.02f;
-
         fontTitle.draw(sb, layout, titleX, titleY);
-        sb.end();
+    }
+    private void renderOptions(SpriteBatch sb){
+        for (Option option : options){
+            option.render(sb);
+        }
     }
 
     @Override
     public void dispose() {
         optionsMenu.dispose();
+        xBtn.dispose();
+        onTexture.dispose();
+        offTexture.dispose();
+        for (Option option : options){
+            option.dispose();
+        }
     }
 }
